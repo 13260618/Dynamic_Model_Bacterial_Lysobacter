ff491f2c022ceb3c2e89cde0897b582d526e3087
13260618 <111941844+13260618@users.noreply.github.com>
2022-10-25 13:25:55 -0500

    Update sir_model.m

--- sir_model.m
+++ sir_model.m
@@ -1,27 +1,9 @@
-%%%%%  Variables del modelo  %%%%
-
-
-beta =1.36;  % Tasa de transmisión  % beta
-gamma =0.31; % Tasa de recuperación % gamma
-N = 1000;                          % Total de población
-I0 = 10;                           % Número de infectados iniciales
-S0 = 990;                          % Número de Susceptibles iniciales
-T = 20;                            % Tiempo de observación (Semanas)
-R0=beta/gamma;  
-
-%%%%% Valores iniciales y declaración de la función ODE45 %%%%%
-
-y0 = [S0; I0; R0];             % Condiciones iniciales
-tspan = [0 T];                 % Intervalo de integración usando ode45
-[t,y] = ode45(@(t,y) sir_model(t,y,beta,gamma,N),tspan,y0);  %funcion ode45
-
-%%%%% Gráfico  %%%%%
-
-plot1 = plot(t,y);
-xlabel('Weeks'); 
-ylabel('Number of individuals');
-legend('S','I','R','location', 'best');
-title('SIR Model Predictions');
-set(plot1(1),'DisplayName','S','LineWidth',1.5,'Color',[0 0 0]);
-set(plot1(2),'DisplayName','I','LineStyle','--','LineWidth',1.5,'Color',[0 0 0]);
-set(plot1(3),'DisplayName','R','LineStyle',':','LineWidth',1.5,'Color',[0 0 0]);
+function dydt = sir_model(t,y,beta,gamma,N)
+  S = y(1);
+  I = y(2);
+  
+  dS = (-beta*S*I/N) ;
+  dI = (beta*S*(I/N) - gamma*I);
+  dR = (gamma*I);
+  dydt = [dS;dI;dR];
+end
