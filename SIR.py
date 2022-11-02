e60727bf5b6521d54e812d62261d3e8d5369d4fd
13260618 <111941844+13260618@users.noreply.github.com>
2022-11-01 18:15:49 -0600

    Update SIR.py

--- SIR.py
+++ SIR.py
@@ -1,7 +1,7 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
- 
+
 # Población total
 N = 1000
 # Condiciones iniciales de plantas infectadas y recuperadas.
@@ -11,8 +11,8 @@ S0 = N - I0 - R0
 # Tasas de contagio y de recuperación
 beta, gamma = 1, 0.31
 # Tiempo de simulación
-t=np.linspace(0, 20, 20)
- 
+t = np.linspace(0, 20, 20)
+
 # Ecuaciones diferenciales del modelo
 def deriv(y, t, N, beta, gamma):
     S, I, R = y
@@ -21,15 +21,15 @@ def deriv(y, t, N, beta, gamma):
     dRdt = gamma * I
     return dSdt, dIdt, dRdt
 
- 
-y0 =S0, I0, R0
+
+y0 = S0, I0, R0
 ret = odeint(deriv, y0, t, args=(N, beta, gamma))  #integración con el método ODE en el intervalo t
 S, I, R = ret.T
- 
+
 # Gráficos de S(t), I(t) y R(t)
-fig = plt.figure()  
-ax = fig.add_subplot(111, axisbelow=True) 
-ax.plot(t, S/1000,  alpha=0.5, lw=1.5, label='Susceptibles')  
+fig = plt.figure()#facecolor='w'
+ax = fig.add_subplot(111, axisbelow=True)#facecolor='#dddddd',
+ax.plot(t, S/1000,  alpha=0.5, lw=1.5, label='Susceptibles') # 'black',linestyle = 'dashed',
 ax.plot(t, I/1000,  alpha=0.5, lw=1.5, label='Infectados')
 ax.plot(t, R/1000,  alpha=0.5, lw=1.5, label='Recuperados con inmunidad')
 ax.set_xlabel('Días')
@@ -37,4 +37,3 @@ ax.set_ylabel('Población (1000s)')
 plt.title("Predicciones del modelo SIR con Python")
 legend = ax.legend()
 plt.show()
-
