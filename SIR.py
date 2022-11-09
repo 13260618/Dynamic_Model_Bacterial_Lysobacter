237cbc677eb5629b48b28da2b663a0345e1c8b13
Israel Monjaraz <111941844+13260618@users.noreply.github.com>
2022-11-09 06:47:59 -0600

    Update SIR.py

--- SIR.py
+++ SIR.py
@@ -4,14 +4,14 @@ import matplotlib.pyplot as plt
 
 # Población total
 N = 1000
-# Condiciones iniciales de plantas infectadas y recuperadas.
-I0, R0 = 1, 0
-# Población de plantas susceptibles
-S0 = N - I0 - R0
 # Tasas de contagio y de recuperación
 beta, gamma = 1, 0.31
+# Condiciones iniciales de plantas infectadas y recuperadas.
+I0, R0 = 10, beta/gamma
+# Población de plantas susceptibles
+S0 = N - I0
 # Tiempo de simulación
-t = np.linspace(0, 20, 20)
+t = np.linspace(0, 20)
 
 # Ecuaciones diferenciales del modelo
 def deriv(y, t, N, beta, gamma):
@@ -21,19 +21,18 @@ def deriv(y, t, N, beta, gamma):
     dRdt = gamma * I
     return dSdt, dIdt, dRdt
 
-
 y0 = S0, I0, R0
 ret = odeint(deriv, y0, t, args=(N, beta, gamma))  #integración con el método ODE en el intervalo t
 S, I, R = ret.T
 
-# Gráficos de S(t), I(t) y R(t)
+
 fig = plt.figure()#facecolor='w'
 ax = fig.add_subplot(111, axisbelow=True)#facecolor='#dddddd',
-ax.plot(t, S,  alpha=0.5, lw=1.5, label='Susceptibles') # 'black',linestyle = 'dashed',
-ax.plot(t, I,  alpha=0.5, lw=1.5, label='Infectados')
-ax.plot(t, R,  alpha=0.5, lw=1.5, label='Recuperados con inmunidad')
-ax.set_xlabel('Días')
-ax.set_ylabel('Población')
-plt.title("Predicciones del modelo SIR con Python")
+ax.plot(t, S,  alpha=0.5, lw=2, label='S') # 'black',linestyle = 'dashed',
+ax.plot(t, I,  alpha=0.5, lw=2, label='I')
+ax.plot(t, R,  alpha=0.5, lw=2, label='R')
+ax.set_xlabel('Semanas',fontsize = 12)
+ax.set_ylabel('Población',fontsize = 12)
+plt.title("Predicciones del modelo SIR con Python",fontsize = 14)
 legend = ax.legend()
 plt.show()
